96a41383602c8858fe055b1e6c90f34da7d193d7
spookyexe <gianepanto@gmail.com>
2024-09-24 21:13:00 +0800

    added admission page cards, still has a lot of bugs

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 
-const Card = ({ className, url, height, width, color }) => {
+const Card = ({ className, url, color }) => {
   return (
     <div
-      className={`bg-${color} h-${height} lg:w-${width} w-[90%] rounded-3xl ${className}`}
+      className={`bg-${color} h-96 lg:w-64 w-[90%] rounded-3xl ${className}`}
       style={{
         backgroundImage: `url(${url})`,
         backgroundSize: "cover",
